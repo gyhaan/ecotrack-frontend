6e77ebd82ea4b2fb01508f0aa2c144d9513b9d63
l-isaro <l.isaro@alustudent.com>
2024-06-17 20:10:16 +0200

    feat: Add AdminUserSearch component with household/collector toggle and search input

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,10 +1,13 @@
+import AdminUserSearch from "../UI/Admin/AdminUserSearch";
+
 function Admin() {
   return (
-    <div>
-      <h1 className="text-green font-bold underline underline-offset-2">
-        Hello, World
-      </h1>
+   
+    <div className="flex flex-col items-center gap-4 w-fit font-body">
+      <h3 className="font-bold text-xl">Welcome to EcoTrack, Admin</h3>
+      <AdminUserSearch/>
     </div>
+  
   );
 }
 
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,13 +2,13 @@ import { Outlet } from "react-router";
 
 function AppLayout() {
   return (
-    <div className="max-w-7xl mx-auto px-5 lg:px-10 py-4  h-full">
+    <div className="mx-auto px-5 lg:px-10 py-4 max-w-7xl h-full">
       <header className="mb-8">
         <div className="flex justify-center">
           <img src="/Logo.svg" alt="logo" />
         </div>
       </header>
-      <main>
+      <main className="flex flex-col items-center">
         <Outlet />
       </main>
     </div>

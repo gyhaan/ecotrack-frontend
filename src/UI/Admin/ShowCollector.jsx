eb43ca909a5607202d7005c8dedbebd128596a8b
l-isaro <l.isaro@alustudent.com>
2024-06-20 21:50:58 +0200

    feat: Add Show Collector component for displaying and filtering collectors

---
+++ src/UI/Admin/ShowCollector.jsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+
+const fakeData = [
+  {
+    id: 0,
+    allocated_area: "string",
+    user_id: 0,
+    collection_dates: [
+      {
+        id: 0,
+        collection_date: "2024-06-20",
+      },
+    ],
+  },
+];
+function ShowCollector({ collectors, query }) {
+  const results = collectors?.filter((el) => {
+    if (!query) {
+      return el;
+    }
+
+    return (
+      el.id.toString().includes(query) ||
+      el.allocated_area.toLowerCase().includes(query.toLowerCase())
+    );
+  });
+  return (
+    <div>
+      {results?.length ? (
+        <ul className="flex flex-col gap-2 list-disc list-inside">
+          {results?.map((el) => (
+            <li key={el.id} className="text-xl">
+              <Link to={`collector/${el.id}`}>
+                {el.id}-{el.allocated_area}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-xl text-center font-medium text-green mt-3">
+          No Collector Found
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default ShowCollector;

11f6491b1043baa48e46f03d09e89f554633043d
gyhaan <144003379+gyhaan@users.noreply.github.com>
2024-06-21 09:32:10 +0200

    Update Collectors.jsx

--- src/pages/Collectors.jsx
+++ src/pages/Collectors.jsx
@@ -1,14 +1,14 @@
-import CollectorDatePicker from '../UI/Collector/CollectorDatePicker';
-import CollectorPendingCollection from '../UI/Collector/CollectorPendingCollection';
-import CollectorCompletedCollection from '../UI/Collector/CollectorCompletedCollection';
+import CollectorDatePicker from "../UI/Collector/CollectorDatePicker";
+import CollectorPendingCollection from "../UI/Collector/CollectorPendingCollection";
 
 function Collectors() {
-  return <div className='font-body flex flex-col gap-5'>
-    <h3 className="font-bold text-xl">Welcome to EcoTrack, Collector</h3>
-    <CollectorDatePicker />
-    <CollectorPendingCollection />
-    <CollectorCompletedCollection />
-  </div>;
+  return (
+    <div className="font-body flex flex-col gap-5">
+      <h3 className="font-bold text-xl">Welcome to EcoTrack, Collector</h3>
+      <CollectorDatePicker />
+      <CollectorPendingCollection />
+    </div>
+  );
 }
 
 export default Collectors;
